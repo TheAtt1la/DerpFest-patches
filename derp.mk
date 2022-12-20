3e3c675f025d0b2e976ff921e675edb4cf579f69
Alita <116470315+TheAtt1la@users.noreply.github.com>
2022-12-20 10:49:21 -0300

    Update derp.mk

--- derp.mk
+++ derp.mk
@@ -3,14 +3,14 @@ $(call inherit-product, vendor/derp/config/BoardConfigDerpFest.mk)
 $(call inherit-product, vendor/derp/config/BoardConfigSoong.mk)
 $(call inherit-product, device/derp/sepolicy/common/sepolicy.mk)
 -include vendor/derp/build/core/config.mk
+                           
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+SELINUX_IGNORE_NEVERALLOWS := true
 
 TARGET_NO_KERNEL_OVERRIDE := true                                       
 TARGET_NO_KERNEL_IMAGE := true
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true                           
-TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
-SELINUX_IGNORE_NEVERALLOWS := true
 
 TARGET_BOOT_ANIMATION_RES := 1080
-USE_LEGACY_BOOTANIMATION := true
 #WITH_GMS := true
 
